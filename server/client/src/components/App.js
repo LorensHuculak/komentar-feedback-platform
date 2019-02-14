10782537670ed1a868bec4ee216d64f52048c32d
LorensHuculak <huculaklorens@yahoo.com>
2019-02-14 17:25:01 +0100

    Routing Configuration

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//Dummy Components
+const Header = () => <h2>Header</h2>;
+const Landing = () => <h2>Landing</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>Create New</h2>;
 
 class App extends Component {
   render() {
-    return <div>Hi there</div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/surveys' component={Dashboard} />
+            <Route path='/surveys/new/' component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
