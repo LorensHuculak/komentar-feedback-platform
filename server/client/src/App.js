08546465afd049a788b3bd39f338e893a203668b
LorensHuculak <huculaklorens@yahoo.com>
2019-02-14 16:11:51 +0100

    Front End Development Setup

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -5,20 +5,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p />
+          Edit <code>src/App.js</code> and save to reload.
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'>
             Learn React
           </a>
+          <a href='/auth/google'>Sign in with Google</a>
         </header>
       </div>
     );
